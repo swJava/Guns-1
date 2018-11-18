0313a2bcff24d4fdfc01959a4dea8f3f3781afe8
hua luo <huahua@huadeMac-mini.local>
2018-11-18 16:33:24 +0800

    log

--- guns-rest/src/main/java/com/stylefeng/guns/rest/GunsRestApplication.java
+++ guns-rest/src/main/java/com/stylefeng/guns/rest/GunsRestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GunsRestApplication {
 
     public static void main(String[] args) {
+        System.out.println("ok");
         SpringApplication.run(GunsRestApplication.class, args);
     }
 }
